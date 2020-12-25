1d11fa99da3cfe772208954f1a22d85aad58d7bf
Jeremy Wood <js.wood206@gmail.com>
2020-12-25 12:24:24 -0800

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Coupon.create(coupon_code: "coupon a", store: "ABC Store")
+Coupon.create(coupon_code: "coupon b", store: "7-11")
+Coupon.create(coupon_code: "coupon c", store: "Albertsons")
+Coupon.create(coupon_code: "coupon d", store: "Wawa")
+Coupon.create(coupon_code: "coupon e", store: "Culvers")
+Coupon.create(coupon_code: "coupon f", store: "In n Out")
+Coupon.create(coupon_code: "coupon g", store: "Kum n Go")
