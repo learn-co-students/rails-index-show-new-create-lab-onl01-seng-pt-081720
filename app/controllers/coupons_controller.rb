2e9289296298a251f55a448c4e7bb0c8e543a670
tonitdiep <adiep123@gmail.com>
2021-01-08 02:52:23 -0500

    Done.

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,14 +1,26 @@
 class CouponsController < ApplicationController
 
     def index
-    end
-    
-    def new
+        @coupons = Coupon.all
         
     end
-
-    def create
+    
+    def show
         # byebug
+        @coupon = Coupon.find(params[:id])
+    end
 
+    def new
+ 
+    end
+
+    def create  #CouponsController POST create with valid attributes/
+        #creates a new coupon & redirects to the new coupon
+        # byebug
+        coupon = Coupon.new
+        coupon[:coupon_code] = params[:coupon][:coupon_code]
+        coupon[:store] = params[:coupon][:store]
+        coupon.save
+        redirect_to coupon_path(coupon)
     end
 end
